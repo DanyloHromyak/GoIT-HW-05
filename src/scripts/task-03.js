cf2e1e338bef76dd6a97f3b7ac6ea15f016fd41f
DanyloHromyak <93078858+DanyloHromyak@users.noreply.github.com>
2022-06-20 18:18:14 +0300

    done

---
+++ src/scripts/task-03.js
@@ -0,0 +1,37 @@
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
+  getItems() {
+    return this.items;
+  }
+  addItem(item) {
+    return this.items.push(item);
+  }
+  removeItem(item) {
+    for (let i = 0; i < this.items.length; i += 1) {
+      if (this.items[i] === item) {
+        this.items.splice(i, 1);
+      }
+    }
+    return this.items;
+  }
+}
+
+const storage = new Storage([
+  'Нанітоіди',
+  'Пролонгер',
+  'Залізні жупи',
+  'Антигравітатор',
+]);
+
+
+
+const items = storage.getItems();
+console.log(items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор" ]
+
+storage.addItem('Дроїд');
+console.log(storage.items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор", "Дроїд" ]
+
+storage.removeItem('Пролонгер');
+console.log(storage.items); // [ "Нанітоіди", "Залізні жупи", "Антигравітатор", "Дроїд" ]
